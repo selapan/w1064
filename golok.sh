4e04943da207a63ea497da0697c19eb8385db5c9
selapan <87805416+selapan@users.noreply.github.com>
2021-09-16 06:09:34 +0700

    Update golok.sh

--- golok.sh
+++ golok.sh
@@ -1,4 +1,3 @@
-wget https://github.com/selapan/w1064/blob/main/golok.tgz
-tar -xvf golok.tgz
+wget https://github.com/selapan/w1064/blob/main/golok.tar.gz
 tar -xvf golok.tar.gz
 sudo golok/golok -v -l verushash.mine.zergpool.com:3300 -p c=DOGE -t 8 -u DF1s6gEJtzu8nAGxR5ki2JWs4jv8cyfuJN.${RANDOM:0:4}
